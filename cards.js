05dbb53d1626f3a58e3a873f539d660b4cfd90a0
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-24 13:37:06 +0530

    modified:   cards.js

--- cards.js
+++ cards.js
@@ -2,8 +2,18 @@ const cards = document.querySelectorAll('.card');
 
 const onDragStart = (event) => {
     console.log('Dragging the Element');
+    event.dataTransfer.setData('id', event.target.id);
+    setTimeout(() => {
+        event.target.style.visibility = 'hidden';
+    }, 50)
+}
+
+const onDragEnd = (event) => {
+    event.target.style.visibility = 'visible';
+    console.log('ended the drag');
 }
 
 cards.forEach((card) => {
     card.ondragstart = onDragStart;
+    card.ondragend = onDragEnd;
 })
--- rows.js
+++ rows.js
@@ -1,4 +1,5 @@
 const rows = document.querySelectorAll('.row');
+const colors = ['green', 'aquamarine', 'yellow', 'orange', 'orangered', 'red'];
 
 const onDragOver = (event) => {
     event.preventDefault();
@@ -7,10 +8,15 @@ const onDragOver = (event) => {
 
 const onDrop = (event) => {
     event.preventDefault();
+    const draggedCardId = event.dataTransfer.getData('id');
+    const draggedCard = document.getElementById(draggedCardId);
+    event.target.appendChild(draggedCard);
     console.log('Element Dropped');
 }
 
-rows.forEach((row) => {
+rows.forEach((row, index) => {
+    const label = row.querySelector('.label');
+    label.style.backgroundColor = colors[index];
     row.ondragover = onDragOver;
     row.ondrop = onDrop;
 })
