be3b0b890b106ab32e2fab80225e622bcda49faf
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-25 00:22:04 +0530

    new file:   assets/video.mp4

--- cards.js
+++ cards.js
@@ -1,67 +1,68 @@
 const cards = document.querySelectorAll('.card');
 const addCard = document.querySelector('#addCard');
 
-const addCardToBoard = (event) => {
-    const card = createCard();
-    const firstRow = document.querySelector('.row');
-    firstRow.appendChild(card);
+const addCardToBank = (event) => {
+  const card = createCard();
+  const bank = document.querySelector('#bank');
+  bank.appendChild(card);
 }
 
-addCard.onclick = addCardToBoard
+addCard.onclick = addCardToBank;
 
 const createCard = () => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.setAttribute('draggable', 'true');
-    card.id = Date.now();
-    card.ondragstart = onDragStart;
-    card.ondragend = onDragEnd;
-    card.onclick = deleteCard;
-    appendImage(card);
-    return card;
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.setAttribute('draggable', 'true');
+  card.id = Date.now();
+  card.ondragstart = onDragStart;
+  card.ondragend = onDragEnd;
+  card.onclick = deleteCard;
+  appendImage(card);
+  return card;
 }
 
 const appendImage = (card) => {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/x-png,image/gif,image/jpeg');
-    input.style.visibility = 'hidden';
-    input.onchange = () => {
-        const image = new Image(100, 100);
-        const file = input.files[0];
-        console.log(file);
-        const reader = new FileReader();
-        reader.onload = (event) => {
-            image.src = event.target.result;
-            image.style.pointerEvents = 'none';
-            card.appendChild(image);
-        }
-        reader.readAsDataURL(file);
+  const input = document.createElement('input');
+  input.setAttribute('type', 'file');
+  input.setAttribute('accept', 'image/x-png,image/gif,image/jpeg');
+  input.style.visibility = 'hidden';
+  input.onchange = () => {
+    const image = new Image(100, 85);
+    const file = input.files[0];
+    console.log(file);
+    
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      image.src = event.target.result;
+      image.style.pointerEvents = 'none';
+      card.appendChild(image);
     }
-    input.click();
+    reader.readAsDataURL(file);
+  }
+  input.click();
 }
 
 const deleteCard = (event) => {
-    const willDeleteCard = window.confirm('Do you really wanna delete this???');
-    if (willDeleteCard) {
-        event.target.remove();
-    }
+  const willDeleteCard = window.confirm('Do you really want to delete this???');
+  if (willDeleteCard) {
+    event.target.remove();
+  }
 }
 
 const onDragStart = (event) => {
-    console.log('Dragging the Element');
-    event.dataTransfer.setData('id', event.target.id);
-    setTimeout(() => {
-        event.target.style.visibility = 'hidden';
-    }, 50)
+  console.log('dragging the element');
+  event.dataTransfer.setData('id', event.target.id);
+  setTimeout(() => {
+    event.target.style.visibility = 'hidden';
+  }, 50)
 }
 
 const onDragEnd = (event) => {
-    event.target.style.visibility = 'visible';
-    console.log('ended the drag');
+  event.target.style.visibility = 'visible';
+  console.log('ended the drag');
 }
 
-cards.forEach((card) => {
-    card.ondragstart = onDragStart;
-    card.ondragend = onDragEnd;
+cards.forEach((card, index) => {
+  card.ondragstart = onDragStart;
+  card.ondragend = onDragEnd;
 })
--- rows.js
+++ rows.js
@@ -2,21 +2,20 @@ const rows = document.querySelectorAll('.row');
 const colors = ['green', 'aquamarine', 'yellow', 'orange', 'orangered', 'red'];
 
 const onDragOver = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 }
 
-
 const onDrop = (event) => {
-    event.preventDefault();
-    const draggedCardId = event.dataTransfer.getData('id');
-    const draggedCard = document.getElementById(draggedCardId);
-    event.target.appendChild(draggedCard);
-    console.log('Element Dropped');
+  event.preventDefault();
+  const draggedCardId = event.dataTransfer.getData('id');
+  const draggedCard = document.getElementById(draggedCardId);
+  event.target.appendChild(draggedCard);
+  console.log('dropped the element');
 }
 
 rows.forEach((row, index) => {
-    const label = row.querySelector('.label');
-    label.style.backgroundColor = colors[index];
-    row.ondragover = onDragOver;
-    row.ondrop = onDrop;
+  const label = row.querySelector('.label');
+  label.style.backgroundColor = colors[index];
+  row.ondragover = onDragOver;
+  row.ondrop = onDrop;
 })
